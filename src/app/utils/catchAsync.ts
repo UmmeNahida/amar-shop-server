f803802462669d8b293c64a563cde3ac3bb902c6
Nahida Akther <ummenahida20@gmail.com>
2026-08-21 13:06:25 +0600

    fix: ensure error is passed to next middleware in catchAsync

--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -2,17 +2,17 @@ import type { NextFunction, Request, Response } from "express";
 import { envVars } from "../confic/env.js"
 
 
-
 type typeAsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>
-
 
 export const catchAsync = (fn: typeAsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((err) => {
 
         if (envVars.node_env === "development") {
             console.log("catch-err", err)
-            next(err)
         }
 
+        next(err)
     })
 }
+
+
