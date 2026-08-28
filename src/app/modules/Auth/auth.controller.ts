3e791d3273052344a7a1b26c1a6be5a4a36dc6e0
Nahida Akther <ummenahida20@gmail.com>
2026-08-28 14:06:16 +0600

    refactor: simplify credentialsLogin function and remove unused code

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -1,54 +1,23 @@
-
+// import httpStatus from "@/constants";
+import { setAuthCookie } from "@/app/utils/setCookie";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import * as AuthService from "./auth.service"
 import httpStatus from "http-status-codes"
-import passport from "passport";
-import { catchAsync } from "../../utils/catchAsync.js";
-import type { NextFunction, Request, Response } from "express";
-import AppError from "../../ErrorHandler/appErrors.js";
-// import { createUserTokens } from "../../utils/userToken.js";
-import { setAuthCookie } from "../../utils/setCookie.js";
-import { sendResponse } from "../../utils/sendResponse.js";
-
-const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
-    passport.authenticate("local", (err: any, user: any, info: any) => {
-
-        if (err) {
-            return next(new AppError(401, info?.message || err));
-        }
-
-        if (!user) {
-            return next(new AppError(401, info?.message || "User does not exist"));
-        }
-
-        // const userToken = createUserTokens(user)
-        //    console.log("userToken",userToken)
-
-        // setAuthCookie(res, userToken)
-
-        // remove password from user object
-        delete user.toObject().password;
-
-        sendResponse(res, {
-            success: true,
-            statusCode: httpStatus.OK,
-            message: "user login successfully",
-            data: {
-                // accessToken: userToken.accessToken,
-                // refreshToken: userToken.refreshToken,
-                user: user
-            }
-
-        })
-
-    }
-
-    )(req, res, next)
 
 
-    // console.log(users)
+
+export const credentialsLogin = catchAsync(async(req,res)=>{
+  const userInfo = req.body;
+  const result = await AuthService.credentialsLogin(userInfo)
+   setAuthCookie(res, {accessToken:result.accessToken, refreshToken:result.refreshToken})
+
+   sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "User registered successfully.",
+    data: result,
+  });
 })
 
 
-export const authController = {
-    credentialsLogin
-}
