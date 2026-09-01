1eb9e268cdc7cf6656f6fcd0958872ca23a655ae
Nahida Akther <ummenahida20@gmail.com>
2026-09-01 10:32:05 +0600

    fix: correct import path for user router

--- src/app/modules/User/user.router.ts
+++ src/app/modules/User/user.router.ts
@@ -1,19 +1,26 @@
 import express from "express";
-import { validateRequest } from "../../middlewares/validateRequest.js";
-import * as UserValidation from "./user.validation.js";
-import * as UserController from "./user.controller.js";
-import { success } from "zod";
-import type { Error } from "mongoose";
-import upload from "../../helper/multer.confic.js";
-import parseBufferToURI from "../../helper/datauri.js";
-import cloudinary from "../../helper/cloudinary.js";
+import * as UserValidation from "./user.validation";
+import * as UserController from "./user.controller";
+import upload from "@/app/helper/multer.confic";
+import { validateRequest } from "@/app/middlewares/validateRequest";
+import { checkAuth } from "@/app/utils/checkAuth";
+import { Role } from "./user.interface";
 
 
 const router = express.Router();
 
 router.post(
   "/register",
-  upload.array("file",5),
+  upload.single("file"),
   validateRequest(UserValidation.createUserValidationSchema),UserController.registerUser);
+
+router.get("/all", UserController.allUsers)
+router.get("/my-profile",checkAuth(Role.ADMIN, Role.CUSTOMER), UserController.myProfile)
+router.patch("/update-profile",checkAuth(Role.ADMIN, Role.CUSTOMER),UserController.updateProfile)
+router.post("/add-address", checkAuth(Role.ADMIN, Role.CUSTOMER),UserController.addAddress)
+router.patch("/update-address/:id", checkAuth(Role.ADMIN,Role.CUSTOMER),UserController.updateAddress)
+router.delete("/delete-address/:id",checkAuth(Role.ADMIN,Role.CUSTOMER),UserController.deleteAddress)
+router.put("/set-default-address/:id",checkAuth(Role.ADMIN,Role.CUSTOMER),UserController.setDefaultAddress)
+
 
 export const UserRoutes = router;
