1c6d76536778095b7651e496b19518b54bbe93b1
Nahida Akther <ummenahida20@gmail.com>
2026-09-07 11:11:38 +0600

    refactor: refactor myCartRouter

--- src/app/modules/MyCart/myCart.route.ts
+++ src/app/modules/MyCart/myCart.route.ts
@@ -2,10 +2,12 @@
 
 import { Router } from "express";
 import * as myCartController from "./myCart.controller"
+import { checkAuth } from "@/app/utils/checkAuth";
+import { Role } from "../User/user.interface";
 
 
 const router = Router()
 
-router.post("addToCart", myCartController.addToCart)
+router.post("/addToCart",checkAuth(Role.ADMIN,Role.CUSTOMER), myCartController.addToCart)
 
-export const productRouter = router;
+export const myCartRouter = router;
