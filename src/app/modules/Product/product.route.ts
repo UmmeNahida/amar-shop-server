5c0b3fd717825799c97a9585fcebd969314911dc
Nahida Akther <ummenahida20@gmail.com>
2026-09-11 00:08:15 +0600

    feat: enhance product route to support file uploads and validation

--- src/app/modules/Product/product.route.ts
+++ src/app/modules/Product/product.route.ts
@@ -1,14 +1,20 @@
 import { Router } from "express";
-import * as productController from "./product.controller"
+import * as productController from "./product.controller";
+import upload from "@/app/helper/multer.confic";
+import { validateRequest } from "@/app/middlewares/validateRequest";
+import { ProductValidation } from "./product.validation";
 
+const router = Router();
 
-const router = Router()
-
-router.post("/add", productController.addProduct)
-router.get("/single", productController.addProduct)
-router.get("/all", productController.getAllProduct)
+router.post(
+  "/add",
+  upload.array("file"),
+  validateRequest(ProductValidation.createProductValidationSchema),
+  productController.addProduct,
+);
+router.get("/single", productController.addProduct);
+router.get("/all", productController.getAllProduct);
 // router.patch("/update", productController.addProduct)
 // router.delete("/delete", productController.addProduct)
-
 
 export const productRouter = router;
