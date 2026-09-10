7ef91efc1c3850d1d887cd64613e598d7450681c
Nahida Akther <ummenahida20@gmail.com>
2026-09-11 00:07:33 +0600

    refactor: remove space

--- src/app/modules/Product/product.controller.ts
+++ src/app/modules/Product/product.controller.ts
@@ -5,7 +5,6 @@ import * as productService from "./product.service"
 
 
 export const addProduct = catchAsync(async(req, res)=>{
-  console.log("req.body:", req.body)
    const file = req.files;
    const result = await productService.createProduct(req.body, file);
    sendResponse(res,{
