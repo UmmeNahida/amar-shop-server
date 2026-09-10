cee44b7c9e32a265bcbdf3e5fe75da3df48a50d7
Nahida Akther <ummenahida20@gmail.com>
2026-09-10 23:49:41 +0600

    feat: add multiple image uploads function

--- src/app/helper/datauri.ts
+++ src/app/helper/datauri.ts
@@ -1,7 +1,32 @@
 import DatauriParser from "datauri/parser.js";
+import cloudinary from "./cloudinary";
+
+export interface IUplodedFiles {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  buffer: string;
+  size: number;
+}
 
 const parser = new DatauriParser();
 
-const parseBufferToURI = (buffer:any) => parser.format("", buffer).content;
+export const parseBufferToURI = (buffer:string) => parser.format("", buffer).content;
 
-export default parseBufferToURI;
+export const uploadedFiles = async (files: IUplodedFiles[]) => {
+  return Promise.all(
+    files.map(async (file) => {
+      const fileString = parseBufferToURI(file.buffer) as string;
+
+      const uploadFile = await cloudinary.uploader.upload(fileString, {
+        folder: "nahida-assets",
+      });
+
+      return {
+        public_id: uploadFile.public_id,
+        url: uploadFile.secure_url,
+      };
+    }),
+  );
+};
