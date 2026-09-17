54278c8a106479129c92ed64d495bf496e115a25
Nahida Akther <ummenahida20@gmail.com>
2026-09-17 12:12:12 +0600

    refactor: optimize response code more afficiently

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -1,24 +1,28 @@
-import type { Request, Response } from "express";
+import type { Response } from "express";
 
-interface IMeta{
-    total: number;
+interface IMeta {
+  page: number;
+  limit: number;
+  totalPage: number;
+  total: number;
 }
 
-interface IResponse<T>{
-    statusCode: number;
-    success:boolean;
-    message: string;
-    data:T;
-    meta?: IMeta
+interface IResponse<T> {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  data: T;
+  meta?: IMeta;
 }
 
-
-export const sendResponse = <T>(res:Response, data: IResponse<T>)=>{
-    
-     res.status(data.statusCode).json({
-        success: data.success,
-        message: data.message,
-        data: data.data,
-        meta: data.meta
-     })
-}
+export const sendResponse = <T>(
+  res: Response,
+  data: IResponse<T>,
+) => {
+  res.status(data.statusCode).json({
+    success: data.success,
+    message: data.message,
+    data: data.data,
+    meta: data.meta,
+  });
+};
