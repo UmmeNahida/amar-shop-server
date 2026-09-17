c1196d881dc4344fdb6738705ea8ec02f3f69202
Nahida Akther <ummenahida20@gmail.com>
2026-09-17 12:37:56 +0600

    style(user-router): format routes with consistent indentation and line breaks

--- src/app/modules/User/user.router.ts
+++ src/app/modules/User/user.router.ts
@@ -6,21 +6,45 @@ import { validateRequest } from "@/app/middlewares/validateRequest";
 import { checkAuth } from "@/app/utils/checkAuth";
 import { Role } from "./user.interface";
 
-
 const router = express.Router();
 
 router.post(
   "/register",
   upload.single("file"),
-  validateRequest(UserValidation.createUserValidationSchema),UserController.registerUser);
+  validateRequest(UserValidation.createUserValidationSchema),
+  UserController.registerUser,
+);
 
-router.get("/all", UserController.allUsers)
-router.get("/my-profile",checkAuth(Role.ADMIN, Role.CUSTOMER), UserController.myProfile)
-router.patch("/update-profile",checkAuth(Role.ADMIN, Role.CUSTOMER),UserController.updateProfile)
-router.post("/add-address", checkAuth(Role.ADMIN, Role.CUSTOMER),UserController.addAddress)
-router.patch("/update-address/:id", checkAuth(Role.ADMIN,Role.CUSTOMER),UserController.updateAddress)
-router.delete("/delete-address/:id",checkAuth(Role.ADMIN,Role.CUSTOMER),UserController.deleteAddress)
-router.put("/set-default-address/:id",checkAuth(Role.ADMIN,Role.CUSTOMER),UserController.setDefaultAddress)
-
+router.get("/all", checkAuth(Role.ADMIN), UserController.allUsers);
+router.get(
+  "/my-profile",
+  checkAuth(Role.ADMIN, Role.CUSTOMER),
+  UserController.myProfile,
+);
+router.patch(
+  "/update-profile",
+  checkAuth(Role.ADMIN, Role.CUSTOMER),
+  UserController.updateProfile,
+);
+router.post(
+  "/add-address",
+  checkAuth(Role.ADMIN, Role.CUSTOMER),
+  UserController.addAddress,
+);
+router.patch(
+  "/update-address/:id",
+  checkAuth(Role.ADMIN, Role.CUSTOMER),
+  UserController.updateAddress,
+);
+router.delete(
+  "/delete-address/:id",
+  checkAuth(Role.ADMIN, Role.CUSTOMER),
+  UserController.deleteAddress,
+);
+router.put(
+  "/set-default-address/:id",
+  checkAuth(Role.ADMIN, Role.CUSTOMER),
+  UserController.setDefaultAddress,
+);
 
 export const UserRoutes = router;
