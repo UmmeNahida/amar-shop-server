d4bc5dfe8b5e7e635c1bb649e0907bdfd028f41e
Nahida Akther <ummenahida20@gmail.com>
2026-09-19 00:36:56 +0600

    feat(brand-service): add reusable query builder to get all category with proper searching and filtering

--- src/app/modules/Brand/brand.service.ts
+++ src/app/modules/Brand/brand.service.ts
@@ -2,19 +2,18 @@ import AppError from "@/app/ErrorHandler/appErrors";
 import { IBrand } from "./brand.interface";
 import { Brand } from "./brand.model";
 import httpStatus from "http-status-codes";
+import { QueryBuilder } from "@/app/utils/QueryBuilder";
+import { BRAND_SEARCH_FIELDS } from "@/app/utils/constand";
 
 export const createBrand = async (payload: IBrand) => {
   const name = payload.name.trim().toLocaleLowerCase();
-  console.log("name",name)
+  console.log("name", name);
   const existingBrand = await Brand.findOne({ name });
 
   if (existingBrand) {
-    throw new AppError(
-      httpStatus.CONFLICT,
-      "Brand already exists"
-    );
+    throw new AppError(httpStatus.CONFLICT, "Brand already exists");
   }
-  
+
   const result = await Brand.create({
     ...payload,
     name,
@@ -23,20 +22,29 @@ export const createBrand = async (payload: IBrand) => {
   return result;
 };
 
-export const deleteBrand = async(deletId:string)=>{
-  const isExistBrand = await Brand.findOne({_id: deletId})
+export const deleteBrand = async (deletId: string) => {
+  const isExistBrand = await Brand.findOne({ _id: deletId });
 
-  if(!isExistBrand){
-      throw new AppError(httpStatus.NOT_FOUND,"Brand is Not found")
+  if (!isExistBrand) {
+    throw new AppError(httpStatus.NOT_FOUND, "Brand is Not found");
   }
 
   const result = await Brand.findByIdAndDelete(deletId);
   return result;
-}
+};
 
+export const getAllBrand = async (query: Record<string, unknown>) => {
+  const brandQuery = new QueryBuilder(Brand.find(), query)
+    .search(BRAND_SEARCH_FIELDS)
+    .filter()
+    .sort()
+    .select()
+    .paginate();
 
-export const getAllBrand = async()=>{
-  const result = await Brand.find();
-  return result;
-}
+  const [data, meta] = await Promise.all([
+    brandQuery.build(),
+    brandQuery.getMeta(),
+  ]);
 
+  return { data, meta };
+};
