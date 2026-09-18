d376a20abc80fdd23d820a73d020fc2da0d4d400
Nahida Akther <ummenahida20@gmail.com>
2026-09-19 00:35:30 +0600

    refactor(brand-controller): improve addBrand, deleteBrand and getBrand code formatting also modify the response

--- src/app/modules/Brand/brand.controller.ts
+++ src/app/modules/Brand/brand.controller.ts
@@ -3,38 +3,37 @@ import * as BrandService from "./brand.service";
 import { sendResponse } from "@/app/utils/sendResponse";
 import httpStatus from "http-status-codes";
 
-
-export const AddBrand = catchAsync(async(req, res)=>{
-   const result = await BrandService.createBrand(req.body)
-   sendResponse(res,{
-    success:true,
+export const AddBrand = catchAsync(async (req, res) => {
+  const result = await BrandService.createBrand(req.body);
+  sendResponse(res, {
+    success: true,
     statusCode: httpStatus.OK,
     message: "Brand has been added successfully",
-    data:result
-   })
-})
+    data: result,
+  });
+});
 
-export const deleteBrand = catchAsync(async(req, res)=>{
-
+export const deleteBrand = catchAsync(async (req, res) => {
   const deletedId = req.params.id as string;
 
-   const result = await BrandService.deleteBrand(deletedId)
-   sendResponse(res,{
-    success:true,
+  const result = await BrandService.deleteBrand(deletedId);
+  sendResponse(res, {
+    success: true,
     statusCode: httpStatus.OK,
     message: "Brand has been deleted successfully",
-    data:result
-   })
-})
+    data: result,
+  });
+});
 
-
-export const getAllBrand = catchAsync(async(req, res)=>{
-
-   const result = await BrandService.getAllBrand()
-   sendResponse(res,{
-    success:true,
+export const getAllBrand = catchAsync(async (req, res) => {
+  const result = await BrandService.getAllBrand(
+    req.query as Record<string, unknown>,
+  );
+  sendResponse(res, {
+    success: true,
     statusCode: httpStatus.OK,
-    message: "All brand is retrieve successfully",
-    data:result
-   })
-})
+    message: "Brands retrieved successfully",
+    data: result.data,
+    meta: result.meta,
+  });
+});
